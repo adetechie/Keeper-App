407f595886db5b8c442b15a751481f58988334c3
adetechie <akinrinlolaadeyemi@gmail.com>
2019-12-23 15:38:49 +0100

    Update changes inside package.json

---
+++ build/precache-manifest.d9d81f4692fea77d6efdfdd758dc14f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0db6c3b67a2cad82559026cc6f47874",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "e0c0e7d8df3e2c444913",
+    "url": "/Keeper-App/static/js/2.7ca76777.chunk.js"
+  },
+  {
+    "revision": "34eac556354114ce1e4c",
+    "url": "/Keeper-App/static/js/main.0460f59c.chunk.js"
+  },
+  {
+    "revision": "b1683c3626c11bd6dba3",
+    "url": "/Keeper-App/static/js/runtime-main.8a2436c1.js"
+  }
+]);

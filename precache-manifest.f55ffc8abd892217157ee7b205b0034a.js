184a53b720037e2290322426876d01ca10b9c653
adetechie <akinrinlolaadeyemi@gmail.com>
2019-12-23 16:00:15 +0100

    Updates

---
+++ precache-manifest.f55ffc8abd892217157ee7b205b0034a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2099a8f75220feb1b8c2aee817d226b5",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "e0c0e7d8df3e2c444913",
+    "url": "/Keeper-App/static/js/2.7ca76777.chunk.js"
+  },
+  {
+    "revision": "34eac556354114ce1e4c",
+    "url": "/Keeper-App/static/js/main.0460f59c.chunk.js"
+  },
+  {
+    "revision": "b1683c3626c11bd6dba3",
+    "url": "/Keeper-App/static/js/runtime-main.8a2436c1.js"
+  }
+]);
